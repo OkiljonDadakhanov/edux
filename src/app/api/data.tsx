1a75561aec0ed47fe8b04e401d918af52198cd25
Okiljon <okiljondadakhonov@gmail.com>
2025-12-16 17:20:59 +0500

    fixed some imgs displaying on link

--- src/app/api/data.tsx
+++ src/app/api/data.tsx
@@ -10,7 +10,7 @@ export const courseData: {
   {
     heading: "Matematika: Algebra asoslari (4-sinf)",
     name: "Nodira Karimova",
-    imgSrc: "/images/courses/math.jpg",
+    imgSrc: "images/courses/math.jpg",
     students: 150,
     classes: 12,
     price: 20,
@@ -19,7 +19,7 @@ export const courseData: {
   {
     heading: "Fizika: Kuch va harakat (5-sinf)",
     name: "Nodira Karimova",
-    imgSrc: "/images/courses/fizika.jpg",
+    imgSrc: "images/courses/fizika.jpeg",
     students: 130,
     classes: 12,
     price: 20,
@@ -28,7 +28,7 @@ export const courseData: {
   {
     heading: "Biologiya: Hayvonlar dunyosi (6-sinf)",
     name: "Dilshod Qodirov",
-    imgSrc: "/images/courses/biology.jpg",
+    imgSrc: "images/courses/biology.jpg",
     students: 120,
     classes: 12,
     price: 20,
@@ -37,7 +37,7 @@ export const courseData: {
   {
     heading: "Kimyo: Asosiy reaksiyalar (7-sinf)",
     name: "Jahongir Akramov",
-    imgSrc: "/images/courses/chemistry.jpg",
+    imgSrc: "images/courses/chemistry.jpg",
     students: 150,
     classes: 12,
     price: 20,
@@ -46,7 +46,7 @@ export const courseData: {
   {
     heading: "Matematika: Geometriya (8-sinf)",
     name: "Nodira Karimova",
-    imgSrc: "/images/courses/math.jpg",
+    imgSrc: "images/courses/math.jpg",
     students: 150,
     classes: 12,
     price: 20,
@@ -55,16 +55,16 @@ export const courseData: {
   {
     heading: "Fizika: Elektr va magnit (9-sinf)",
     name: "Dilshod Qodirov",
-    imgSrc: "/images/courses/coursethree.png",
+    imgSrc: "images/courses/coursethree.png",
     students: 150,
     classes: 12,
     price: 20,
     rating: 4.2,
   },
   {
-    heading: "Biologiya: O‘simliklar dunyosi (10-sinf)",
+    heading: "Biologiya: O'simliklar dunyosi (10-sinf)",
     name: "Nodira Karimova",
-    imgSrc: "/images/courses/biology.jpg",
+    imgSrc: "images/courses/biology.jpg",
     students: 150,
     classes: 12,
     price: 20,
@@ -73,7 +73,7 @@ export const courseData: {
   {
     heading: "Kimyo: Organik kimyo asoslari (11-sinf)",
     name: "Jahongir Akramov",
-    imgSrc: "/images/courses/chemistry.jpg",
+    imgSrc: "images/courses/chemistry.jpg",
     students: 150,
     classes: 12,
     price: 20,
@@ -82,41 +82,41 @@ export const courseData: {
 ];
 
 export const MentorData: { profession: string; name: string; imgSrc: string }[] = [
-  { profession: "Matematika o‘qituvchisi", name: "Shavkat Yusupov", imgSrc: "/images/mentor/user3.png" },
-  { profession: "Fizika o‘qituvchisi", name: "Dilshod Qodirov", imgSrc: "/images/mentor/user2.png" },
-  { profession: "Biologiya o‘qituvchisi", name: "Nodira Karimova", imgSrc: "/images/mentor/user1.png" },
-  { profession: "Kimyo o‘qituvchisi", name: "Jahongir Akramov", imgSrc: "/images/mentor/user3.png" },
-  { profession: "Matematika o‘qituvchisi", name: "Shavkat Yusupov", imgSrc: "/images/mentor/user2.png" },
-  { profession: "Fizika o‘qituvchisi", name: "Dilshod Qodirov", imgSrc: "/images/mentor/user1.png" },
+  { profession: "Matematika o'qituvchisi", name: "Shavkat Yusupov", imgSrc: "images/mentor/user3.png" },
+  { profession: "Fizika o'qituvchisi", name: "Dilshod Qodirov", imgSrc: "images/mentor/user2.png" },
+  { profession: "Biologiya o'qituvchisi", name: "Nodira Karimova", imgSrc: "images/mentor/user1.png" },
+  { profession: "Kimyo o'qituvchisi", name: "Jahongir Akramov", imgSrc: "images/mentor/user3.png" },
+  { profession: "Matematika o'qituvchisi", name: "Shavkat Yusupov", imgSrc: "images/mentor/user2.png" },
+  { profession: "Fizika o'qituvchisi", name: "Dilshod Qodirov", imgSrc: "images/mentor/user1.png" },
 ];
 
 export const TestimonialData: { profession: string; comment: string; imgSrc: string; name: string; rating: number }[] = [
   {
     name: "Ali Xasanov",
-    profession: "O‘quvchi, 10-sinf",
-    comment: "Matematika bo‘yicha kurs juda foydali bo‘ldi, darslar tushunarli va interaktiv edi.",
-    imgSrc: "/images/testimonial/user.svg",
+    profession: "O'quvchi, 10-sinf",
+    comment: "Matematika bo'yicha kurs juda foydali bo'ldi, darslar tushunarli va interaktiv edi.",
+    imgSrc: "images/testimonial/user.svg",
     rating: 5,
   },
   {
     name: "Laylo Rahimova",
-    profession: "O‘quvchi, 9-sinf",
+    profession: "O'quvchi, 9-sinf",
     comment: "Fizika mavzulari sodda va qiziqarli tushuntirilgan. Har bir topshiriq amaliy edi.",
-    imgSrc: "/images/mentor/user2.png",
+    imgSrc: "images/mentor/user2.png",
     rating: 5,
   },
   {
     name: "Jasur Omonov",
-    profession: "O‘quvchi, 11-sinf",
+    profession: "O'quvchi, 11-sinf",
     comment: "Kimyo kursi ajoyib! Har bir mavzu misollar bilan tushuntirilgan.",
-    imgSrc: "/images/mentor/user3.png",
+    imgSrc: "images/mentor/user3.png",
     rating: 5,
   },
   {
     name: "Nilufar Karimova",
-    profession: "O‘quvchi, 8-sinf",
+    profession: "O'quvchi, 8-sinf",
     comment: "Biologiya darslari juda qiziqarli va interaktiv edi.",
-    imgSrc: "/images/mentor/user1.png",
+    imgSrc: "images/mentor/user1.png",
     rating: 5,
   },
 ];
